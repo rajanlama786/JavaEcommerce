7c6b2c8799eb73936552f5a2461a5b83ad1e6a57
Rajan Lama <rajan.lama786@gmail.com>
2020-09-06 21:45:51 +0545

    project updated to java ecommerce project

---
+++ src/test/java/com/JavaEcommerce/JavaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
